d4562b201c6c668d529ec0a516f22e1622602a22
Admin <dohonghanh2101@gmail.com>
2020-12-11 22:12:27 +0700

    update

---
+++ Ice-Cream-master/Ice_Cream/Controllers/AboutController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ice_Cream.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+       
+    
+}
